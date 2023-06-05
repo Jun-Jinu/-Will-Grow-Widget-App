598835257315acaf55b0adfedb0f3d0784103685
ForwardOoh <soa9719@naver.com>
2023-06-05 23:52:57 +0900

    feat: 일기 모델 추가

---
+++ lib/data/model/home_widget.dart
@@ -0,0 +1,16 @@
+class HomeWidget {
+  final String title;
+  final String backgroundColor;
+  final String fontColor;
+  final DateTime date;
+
+  HomeWidget({
+    required this.title,
+    required this.backgroundColor,
+    required this.fontColor,
+    required this.date,
+  });
+}
+
+// 위젯 세팅
+// 일기(다짐)
---
+++ lib/data/model/post.dart
@@ -0,0 +1,15 @@
+class Post {
+  final String title;
+  final String content;
+  final String summary;
+  final DateTime summaryEndDate;
+  final DateTime date;
+
+  Post({
+    required this.title,
+    required this.content,
+    required this.summary,
+    required this.summaryEndDate,
+    required this.date,
+  });
+}
