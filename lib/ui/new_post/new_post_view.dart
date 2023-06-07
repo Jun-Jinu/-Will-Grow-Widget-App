daca9f576ba839e3fc9f6aecb8112e0d4db01558
ForwardOoh <soa9719@naver.com>
2023-06-07 11:27:29 +0900

    feat: 일기 쓰기 페이지 Ui 구성

--- lib/ui/new_post/new_post_view.dart
+++ lib/ui/new_post/new_post_view.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:board_widget/data/repository/post_repository.dart';
 
 import './new_post_viewmodel.dart';
 
 import 'package:board_widget/ui/widgets/menu_bottom.dart';
 
 class NewPostView extends StatelessWidget {
-  NewPostView({super.key});
+  NewPostView({Key? key}) : super(key: key);
 
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _contentController = TextEditingController();
@@ -20,7 +19,7 @@ class NewPostView extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('오늘 일기'),
+        title: Text('오늘의 일기'),
       ),
       bottomNavigationBar: MenuBottom(
         selectedIndex: 1,
@@ -32,50 +31,100 @@ class NewPostView extends StatelessWidget {
             TextField(
               controller: _titleController,
               decoration: InputDecoration(
-                labelText: '제목',
-                hintText: "필수사항",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(width: 0, style: BorderStyle.none),
+                ),
+                hintText: "제목(필수)",
               ),
             ),
-            SizedBox(height: 16.0),
+            Container(
+              width: double.infinity,
+              height: 1,
+              color: Colors.grey,
+              margin: EdgeInsets.symmetric(vertical: 4.0),
+            ),
             TextField(
               controller: _contentController,
               decoration: InputDecoration(
-                labelText: '일기',
-                hintText: "선택사항",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(width: 0, style: BorderStyle.none),
+                ),
+                hintText: "일기(선택)",
               ),
-              maxLines: null,
+              maxLines: 10,
             ),
-            SizedBox(height: 16.0),
+            Container(
+              width: double.infinity,
+              height: 1,
+              color: Colors.grey,
+              margin: EdgeInsets.symmetric(vertical: 4.0),
+            ),
             TextField(
               controller: _promiseController,
               decoration: InputDecoration(
-                labelText: '오늘의 다짐',
-                hintText: "필수사항",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(width: 0, style: BorderStyle.none),
+                ),
+                hintText: "오늘의 다짐(필수)",
               ),
             ),
-
-            // TODO: 며칠간 할지 적기! (일 수 입력받는 빈칸 추가)
-
-            SizedBox(height: 32.0),
-            SizedBox(
+            Container(
               width: double.infinity,
-              height: 40,
-              child: OutlinedButton(
-                onPressed: () {
-                  // Handle button press
-                },
-                style: OutlinedButton.styleFrom(
-                  side: BorderSide(
-                    color: Colors.lightBlue,
-                    width: 1.0,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+              height: 1,
+              color: Colors.grey,
+              margin: EdgeInsets.symmetric(vertical: 4.0),
+            ),
+            Row(
+              children: [
+                SizedBox(width: 8.0),
+                Expanded(
+                  flex: 3,
+                  child: Text(
+                    '얼마만에 이뤄볼까요?',
+                    style: TextStyle(fontSize: 16),
                   ),
                 ),
-                child: Text(
-                  '저장하기',
-                  style: TextStyle(fontSize: 20),
+                Expanded(
+                  flex: 1,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 4.0, horizontal: 12.0),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '일',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 40.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 40,
+                child: OutlinedButton(
+                  onPressed: () {
+                    // Handle button press
+                  },
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(
+                      color: Colors.lightBlue,
+                      width: 1.0,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: Text(
+                    'Save',
+                    style: TextStyle(fontSize: 20),
+                  ),
                 ),
               ),
             ),
