a94f556a2d456602f316e6404a6933154da2c639
ForwardOoh <soa9719@naver.com>
2023-06-08 13:00:11 +0900

    feat: 일기 달력 추가

--- lib/ui/new_post/new_post_view.dart
+++ lib/ui/new_post/new_post_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import './new_post_viewmodel.dart';
 
@@ -13,7 +14,6 @@ class NewPostView extends StatefulWidget {
 }
 
 class _NewPostViewState extends State<NewPostView> {
-  final TextEditingController _titleController = TextEditingController();
   final TextEditingController _contentController = TextEditingController();
   final TextEditingController _promiseController = TextEditingController();
   late NewPostViewModel viewModel;
@@ -22,8 +22,22 @@ class _NewPostViewState extends State<NewPostView> {
   bool onDirect = false;
 
   @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     viewModel = Provider.of<NewPostViewModel>(context);
+
+    DateTime selectedDay = DateTime(
+      DateTime.now().year,
+      DateTime.now().month,
+      DateTime.now().day,
+    );
+
+    DateTime focusedDay = DateTime.now();
+    List<String> days = ['_', '월', '화', '수', '목', '금', '토', '일'];
 
     return Scaffold(
       appBar: AppBar(
@@ -32,145 +46,155 @@ class _NewPostViewState extends State<NewPostView> {
       bottomNavigationBar: MenuBottom(
         selectedIndex: 0,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            // TextField(
-            //   controller: _titleController,
-            //   decoration: InputDecoration(
-            //     border: OutlineInputBorder(
-            //       borderSide: BorderSide(width: 0, style: BorderStyle.none),
-            //     ),
-            //     hintText: "제목(필수)",
-            //   ),
-            // ),
-            // Container(
-            //   width: double.infinity,
-            //   height: 1,
-            //   color: Colors.grey,
-            //   margin: EdgeInsets.symmetric(vertical: 4.0),
-            // ),
-            TextField(
-              controller: _contentController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(width: 0, style: BorderStyle.none),
-                ),
-                hintText: "일기(선택)",
-              ),
-              maxLines: 10,
-            ),
-            Container(
-              width: double.infinity,
-              height: 1,
-              color: Colors.grey,
-              margin: EdgeInsets.symmetric(vertical: 4.0),
-            ),
-            TextField(
-              controller: _promiseController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(width: 0, style: BorderStyle.none),
-                ),
-                hintText: "오늘의 다짐(필수)",
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              height: 1,
-              color: Colors.grey,
-              margin: EdgeInsets.symmetric(vertical: 4.0),
-            ),
-            Row(
-              children: [
-                SizedBox(width: 8.0),
-                Container(
-                  width: 100,
-                  child: DropdownButtonFormField<int>(
-                    value: promiseDuration,
-                    alignment: AlignmentDirectional.centerEnd,
-                    items: [
-                      DropdownMenuItem<int>(
-                        value: 1,
-                        child: Center(child: Text('1')),
-                      ),
-                      DropdownMenuItem<int>(
-                        value: 7,
-                        child: Center(child: Text('7')),
-                      ),
-                      DropdownMenuItem<int>(
-                        value: 30,
-                        child: Center(child: Text('30')),
-                      ),
-                      DropdownMenuItem<int>(
-                        value: -2,
-                        child: Center(child: Text('무제한')),
-                      ),
-                      DropdownMenuItem<int>(
-                        value: -3,
-                        child: Center(child: Text('직접입력')),
-                      ),
-                    ],
-                    onChanged: (value) {
-                      setState(() {
-                        value == -3 ? onDirect = true : onDirect = false;
-                      });
-                    },
-                  ),
-                ),
-                if (onDirect)
-                  Container(
-                    width: 80,
-                    height: 40,
-                    margin: EdgeInsets.only(left: 30.0, right: 5.0),
-                    child: TextField(
-                      maxLines: 1,
-                      maxLength: 5,
-                      keyboardType: TextInputType.number,
-                      textAlign: TextAlign.right,
-                      decoration: InputDecoration(
-                        counterText: "",
-                        border: OutlineInputBorder(),
-                        labelStyle: TextStyle(fontSize: 14),
-                      ),
-                    ),
-                  ),
-                Container(
-                  width: 120,
-                  child: Text(
-                    '일의 다짐',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 40.0),
-              child: SizedBox(
-                width: double.infinity,
-                height: 40,
-                child: OutlinedButton(
-                  onPressed: () {
-                    // Handle button press
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TableCalendar(
+                // locale: 'ko_KR',
+                firstDay: DateTime.utc(2021, 10, 16),
+                lastDay: DateTime.utc(2030, 3, 14),
+                focusedDay: focusedDay,
+                onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+                  // 선택된 날짜의 상태를 갱신합니다.
+                  setState(() {
+                    selectedDay = selectedDay;
+                    focusedDay = focusedDay;
+                  });
+                },
+                selectedDayPredicate: (DateTime day) {
+                  // selectedDay 와 동일한 날짜의 모양을 바꿔줍니다.
+                  return isSameDay(selectedDay, day);
+                },
+                calendarBuilders: CalendarBuilders(
+                  dowBuilder: (context, day) {
+                    return Center(child: Text(days[day.weekday]));
                   },
-                  style: OutlinedButton.styleFrom(
-                    side: BorderSide(
-                      color: Colors.lightBlue,
-                      width: 1.0,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              TextField(
+                controller: _contentController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 0, style: BorderStyle.none),
+                  ),
+                  hintText: "일기",
+                ),
+                maxLines: 10,
+              ),
+              Container(
+                width: double.infinity,
+                height: 1,
+                color: Colors.grey,
+                margin: EdgeInsets.symmetric(vertical: 4.0),
+              ),
+              TextField(
+                controller: _promiseController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 0, style: BorderStyle.none),
+                  ),
+                  hintText: "오늘의 다짐",
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 1,
+                color: Colors.grey,
+                margin: EdgeInsets.symmetric(vertical: 4.0),
+              ),
+              Row(
+                children: [
+                  SizedBox(width: 8.0),
+                  Container(
+                    width: 100,
+                    child: DropdownButtonFormField<int>(
+                      value: promiseDuration,
+                      alignment: AlignmentDirectional.centerEnd,
+                      items: [
+                        DropdownMenuItem<int>(
+                          value: 1,
+                          child: Center(child: Text('1')),
+                        ),
+                        DropdownMenuItem<int>(
+                          value: 7,
+                          child: Center(child: Text('7')),
+                        ),
+                        DropdownMenuItem<int>(
+                          value: 30,
+                          child: Center(child: Text('30')),
+                        ),
+                        DropdownMenuItem<int>(
+                          value: -2,
+                          child: Center(child: Text('무제한')),
+                        ),
+                        DropdownMenuItem<int>(
+                          value: -3,
+                          child: Center(child: Text('직접입력')),
+                        ),
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          value == -3 ? onDirect = true : onDirect = false;
+                        });
+                      },
                     ),
                   ),
-                  child: Text(
-                    '기록하기',
-                    style: TextStyle(fontSize: 20),
+                  if (onDirect)
+                    Container(
+                      width: 80,
+                      height: 40,
+                      margin: EdgeInsets.only(left: 30.0, right: 5.0),
+                      child: TextField(
+                        maxLines: 1,
+                        maxLength: 5,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.right,
+                        decoration: InputDecoration(
+                          counterText: "",
+                          border: OutlineInputBorder(),
+                          labelStyle: TextStyle(fontSize: 14),
+                        ),
+                      ),
+                    ),
+                  Container(
+                    width: 120,
+                    child: Text(
+                      '일의 다짐',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0), // 추가: 달력 아래에 여백을 줍니다.
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 40.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 40,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      // Handle button press
+                    },
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: Colors.lightBlue,
+                        width: 1.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: Text(
+                      '기록하기',
+                      style: TextStyle(fontSize: 20),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
