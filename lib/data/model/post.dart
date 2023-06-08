35a6cfd98957d42276c7117594e8c2346e1f129f
ForwardOoh <soa9719@naver.com>
2023-06-08 21:09:39 +0900

    feat: 포스트 typeAdapter 생성

--- lib/data/model/post.dart
+++ lib/data/model/post.dart
@@ -1,25 +1,55 @@
-class Post {
-  final int id;
-  final String content;
-  final String promise;
-  final DateTime promiseEndDate;
-  final DateTime date;
+import 'package:hive/hive.dart';
 
+// class Post {
+//   final int id;
+//   final String content;
+//   final String promise;
+//   final DateTime date;
+//   final DateTime promiseEndDate;
+
+//   Post({
+//     required this.id,
+//     required this.content,
+//     required this.promise,
+//     required this.promiseEndDate,
+//     required this.date,
+//   });
+
+//   factory Post.fromJson(Map<String, dynamic> json) {
+//     return Post(
+//       id: json['id'],
+//       date: DateTime.parse(json['date']),
+//       content: json['content'],
+//       promise: json['promise'],
+//       promiseEndDate: DateTime.parse(json['promiseEndDate']),
+//     );
+//   }
+// }
+
+part 'post.g.dart';
+
+@HiveType(typeId: 1)
+class Post {
   Post({
     required this.id,
     required this.content,
     required this.promise,
-    required this.promiseEndDate,
     required this.date,
+    required this.promiseEndDate,
   });
 
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-      id: json['id'],
-      date: DateTime.parse(json['date']),
-      content: json['content'],
-      promise: json['promise'],
-      promiseEndDate: DateTime.parse(json['promiseEndDate']),
-    );
-  }
+  @HiveField(0)
+  int id;
+
+  @HiveField(1)
+  String content;
+
+  @HiveField(2)
+  String promise;
+
+  @HiveField(3)
+  DateTime date;
+
+  @HiveField(4)
+  DateTime promiseEndDate;
 }
