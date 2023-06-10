03ca479e06c717046a683dcc5b50b0cc50aa18f4
ForwardOoh <soa9719@naver.com>
2023-06-10 21:31:51 +0900

    refactor: 위젯 설정 함수 분리

--- lib/ui/settings/widget_setting/widget_settings_view.dart
+++ lib/ui/settings/widget_setting/widget_settings_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:settings_ui/settings_ui.dart';
+import './widget_settings_viewmodel.dart';
 
 const BIG_FONT_SIZE = 28.0;
 const NOMAL_FONT_SIZE = 24.0;
@@ -12,57 +14,33 @@ class WidgetSettingView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('위젯 설정'),
       ),
-      body: const WidgetSettingBody(),
+      body: WidgetSettingBody(),
     );
   }
 }
 
-class WidgetSettingBody extends StatefulWidget {
+class WidgetSettingBody extends StatelessWidget {
   const WidgetSettingBody({Key? key}) : super(key: key);
 
   @override
-  _WidgetSettingBodyState createState() => _WidgetSettingBodyState();
-}
-
-class _WidgetSettingBodyState extends State<WidgetSettingBody> {
-  Color fontColor = Colors.white; // 초기 글자색 설정
-  Color backgroundColor = Colors.blueAccent; // 초기 배경색 설정
-  double fontSizeSelection = NOMAL_FONT_SIZE; // 초기 글자 크기 선택 설정
-
-  @override
   Widget build(BuildContext context) {
-    void setBackgroundColor(Color color) {
-      setState(() {
-        backgroundColor = color;
-      });
-    }
-
-    void setFontColor(Color color) {
-      setState(() {
-        fontColor = color;
-      });
-    }
-
-    void setFontSize(double fontSize) {
-      setState(() {
-        fontSizeSelection = fontSize;
-      });
-    }
+    final viewModel = Provider.of<WidgetSettingsViewModel>(context);
 
     return Column(
       children: [
         Container(
           width: double.infinity,
           height: 140,
-          margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
           decoration: BoxDecoration(
-            color: backgroundColor, // 변수로 설정된 배경색 사용
-            borderRadius: BorderRadius.circular(20), // 모서리를 둥글게 설정
+            color: viewModel.backgroundColor,
+            borderRadius: BorderRadius.circular(20),
           ),
           child: Center(
             child: Text(
               '파이팅 해야지!',
-              style: TextStyle(fontSize: fontSizeSelection),
+              style: TextStyle(
+                  fontSize: viewModel.fontSize, color: viewModel.fontColor),
             ),
           ),
         ),
@@ -72,105 +50,134 @@ class _WidgetSettingBodyState extends State<WidgetSettingBody> {
                 const EdgeInsets.symmetric(vertical: 40.0, horizontal: 20.0),
             sections: [
               SettingsSection(
-                margin: EdgeInsetsDirectional.only(bottom: 20.0),
+                margin: const EdgeInsetsDirectional.only(bottom: 20.0),
                 title: const Text('위젯 다짐 변경 주기'),
                 tiles: [
                   SettingsTile.switchTile(
-                    onToggle: (value) {},
-                    initialValue: true,
-                    title: Text('시간별 텍스트 변경'),
-                  ),
-                  SettingsTile(
-                    title: const Text('2시간'),
-                    onPressed: (BuildContext context) {
-                      // Handle contact us settings
+                    onToggle: (value) {
+                      viewModel.toggleTextChangeHourly(value);
                     },
+                    initialValue: viewModel.isTextChangeHourly,
+                    title: const Text('시간별 텍스트 변경'),
                   ),
-                  SettingsTile(
-                    title: const Text('6시간'),
-                    onPressed: (BuildContext context) {
-                      // Handle contact us settings
-                    },
-                  ),
-                  SettingsTile(
-                    title: const Text('12시간'),
-                    onPressed: (BuildContext context) {
-                      // Handle send feedback settings
-                    },
-                  ),
-                  SettingsTile(
-                    title: const Text('하루'),
-                    onPressed: (BuildContext context) {
-                      // Handle share app settings
-                    },
-                  ),
-                  SettingsTile(
-                    title: const Text('일주일'),
-                    onPressed: (BuildContext context) {
-                      // Handle share app settings
-                    },
-                  ),
+                  if (viewModel.isTextChangeHourly) ...[
+                    SettingsTile(
+                      title: const Text('2시간'),
+                      onPressed: (BuildContext context) {
+                        viewModel.onChangeTextChangeHour(2);
+                      },
+                      trailing: viewModel.isTextChangeHour == 2
+                          ? const Icon(Icons.check, color: Colors.blue)
+                          : null,
+                    ),
+                    SettingsTile(
+                      title: const Text('6시간'),
+                      onPressed: (BuildContext context) {
+                        viewModel.onChangeTextChangeHour(6);
+                      },
+                      trailing: viewModel.isTextChangeHour == 6
+                          ? const Icon(Icons.check, color: Colors.blue)
+                          : null,
+                    ),
+                    SettingsTile(
+                      title: const Text('12시간'),
+                      onPressed: (BuildContext context) {
+                        viewModel.onChangeTextChangeHour(12);
+                      },
+                      trailing: viewModel.isTextChangeHour == 12
+                          ? const Icon(Icons.check, color: Colors.blue)
+                          : null,
+                    ),
+                    SettingsTile(
+                      title: const Text('하루'),
+                      onPressed: (BuildContext context) {
+                        viewModel.onChangeTextChangeHour(24);
+                      },
+                      trailing: viewModel.isTextChangeHour == 24
+                          ? const Icon(Icons.check, color: Colors.blue)
+                          : null,
+                    ),
+                    SettingsTile(
+                      title: const Text('일주일'),
+                      onPressed: (BuildContext context) {
+                        viewModel.onChangeTextChangeHour(168);
+                      },
+                      trailing: viewModel.isTextChangeHour == 168
+                          ? const Icon(Icons.check, color: Colors.blue)
+                          : null,
+                    ),
+                  ],
                 ],
               ),
+
               SettingsSection(
-                margin: EdgeInsetsDirectional.only(bottom: 20.0),
+                margin: const EdgeInsetsDirectional.only(bottom: 20.0),
                 title: const Text('위젯 배경색'),
                 tiles: [
                   SettingsTile(
                     title: const Text('흰색'),
                     onPressed: (BuildContext context) {
-                      setBackgroundColor(Colors.white);
+                      viewModel.setBackgroundColor(Colors.white);
                     },
+                    trailing: viewModel.backgroundColor == Colors.white
+                        ? Icon(Icons.check, color: Colors.blue)
+                        : null,
                   ),
                   SettingsTile(
                     title: const Text('회색'),
                     onPressed: (BuildContext context) {
-                      setBackgroundColor(Colors.grey);
+                      viewModel.setBackgroundColor(Colors.grey);
                     },
+                    trailing: viewModel.backgroundColor == Colors.grey
+                        ? Icon(Icons.check, color: Colors.blue)
+                        : null,
                   ),
                   SettingsTile(
                     title: const Text('검정색'),
                     onPressed: (BuildContext context) {
-                      setBackgroundColor(Colors.black);
+                      viewModel.setBackgroundColor(Colors.black);
                     },
+                    trailing: viewModel.backgroundColor == Colors.black
+                        ? Icon(Icons.check, color: Colors.blue)
+                        : null,
                   ),
                 ],
               ),
               SettingsSection(
-                margin: EdgeInsetsDirectional.only(bottom: 20.0),
+                margin: const EdgeInsetsDirectional.only(bottom: 20.0),
                 title: const Text('글자 색'),
                 tiles: [
                   SettingsTile(
                     title: const Text('흰색'),
                     onPressed: (BuildContext context) {
-                      setFontColor(Colors.white);
+                      viewModel.setFontColor(Colors.white);
                     },
-                    trailing: fontColor == Colors.white
+                    trailing: viewModel.fontColor == Colors.white
                         ? Icon(Icons.check, color: Colors.blue)
                         : null,
                   ),
                   SettingsTile(
                     title: const Text('회색'),
                     onPressed: (BuildContext context) {
-                      setFontColor(Colors.grey);
+                      viewModel.setFontColor(Colors.grey);
                     },
-                    trailing: fontColor == Colors.grey
+                    trailing: viewModel.fontColor == Colors.grey
                         ? Icon(Icons.check, color: Colors.blue)
                         : null,
                   ),
                   SettingsTile(
                     title: const Text('검정색'),
                     onPressed: (BuildContext context) {
-                      setFontColor(Colors.black);
+                      viewModel.setFontColor(Colors.black);
                     },
-                    trailing: fontColor == Colors.black
+                    trailing: viewModel.fontColor == Colors.black
                         ? Icon(Icons.check, color: Colors.blue)
                         : null,
                   ),
                 ],
               ),
               SettingsSection(
-                margin: EdgeInsetsDirectional.only(bottom: 20.0),
+                margin: const EdgeInsetsDirectional.only(bottom: 20.0),
                 title: const Text('폰트'),
                 tiles: [
                   SettingsTile(
@@ -189,40 +196,40 @@ class _WidgetSettingBodyState extends State<WidgetSettingBody> {
                 ],
               ),
               SettingsSection(
-                margin: EdgeInsetsDirectional.only(bottom: 20.0),
+                margin: const EdgeInsetsDirectional.only(bottom: 20.0),
                 title: const Text('글자 크기'),
                 tiles: [
                   SettingsTile(
                     title: const Text('크게'),
                     onPressed: (BuildContext context) {
-                      setFontSize(BIG_FONT_SIZE);
+                      viewModel.setFontSize(BIG_FONT_SIZE);
                     },
-                    trailing: fontSizeSelection == BIG_FONT_SIZE
+                    trailing: viewModel.fontSize == BIG_FONT_SIZE
                         ? Icon(Icons.check, color: Colors.blue)
                         : null,
                   ),
                   SettingsTile(
                     title: const Text('보통'),
                     onPressed: (BuildContext context) {
-                      setFontSize(NOMAL_FONT_SIZE);
+                      viewModel.setFontSize(NOMAL_FONT_SIZE);
                     },
-                    trailing: fontSizeSelection == NOMAL_FONT_SIZE
+                    trailing: viewModel.fontSize == NOMAL_FONT_SIZE
                         ? Icon(Icons.check, color: Colors.blue)
                         : null,
                   ),
                   SettingsTile(
                     title: const Text('작게'),
                     onPressed: (BuildContext context) {
-                      setFontSize(SMALL_FONT_SIZE);
+                      viewModel.setFontSize(SMALL_FONT_SIZE);
                     },
-                    trailing: fontSizeSelection == SMALL_FONT_SIZE
+                    trailing: viewModel.fontSize == SMALL_FONT_SIZE
                         ? Icon(Icons.check, color: Colors.blue)
                         : null,
                   ),
                 ],
               ),
               SettingsSection(
-                margin: EdgeInsetsDirectional.only(bottom: 20.0),
+                margin: const EdgeInsetsDirectional.only(bottom: 20.0),
                 title: const Text('날짜 형식'),
                 tiles: [
                   SettingsTile(
--- lib/ui/settings/widget_setting/widget_settings_viewmodel.dart
+++ lib/ui/settings/widget_setting/widget_settings_viewmodel.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
 
-class WidgetSettingsViewModel with ChangeNotifier {
-  bool _darkMode = false;
-  String _selectedLanguage = 'English';
+class WidgetSettingsViewModel extends ChangeNotifier {
+  //TODO: 다 정해지면 MODEL 만들기
+  bool isTextChangeHourly = false;
+  int isTextChangeHour = 24; // 위젯 텍스트 변환 주기
+  Color _fontColor = Colors.white;
+  Color _backgroundColor = Colors.blueAccent;
+  String fontFamily = "";
+  double _fontSize = 24.0;
+  //날짜 표시, 날짜 형식도?
 
-  bool get darkMode => _darkMode;
-  String get selectedLanguage => _selectedLanguage;
+  Color get fontColor => _fontColor;
+  Color get backgroundColor => _backgroundColor;
+  double get fontSize => _fontSize;
 
-  void toggleDarkMode(bool value) {
-    _darkMode = value;
-    // Perform any necessary operations related to toggling dark mode
+  void toggleTextChangeHourly(bool value) {
+    isTextChangeHourly = value;
     notifyListeners();
   }
 
-  void setLanguage(String language) {
-    _selectedLanguage = language;
-    // Perform any necessary operations related to setting the language
+  void onChangeTextChangeHour(int hour) {
+    isTextChangeHour = hour;
+    notifyListeners();
+  }
+
+  void setBackgroundColor(Color color) {
+    _backgroundColor = color;
+    notifyListeners();
+  }
+
+  void setFontColor(Color color) {
+    _fontColor = color;
+    notifyListeners();
+  }
+
+  void setFontSize(double fontSize) {
+    _fontSize = fontSize;
     notifyListeners();
   }
 }
